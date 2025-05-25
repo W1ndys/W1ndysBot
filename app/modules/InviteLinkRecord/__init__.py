86e3b9e19d48f37847a2827b89202c6fd7cec5de
W1ndys <w1ndys@outlook.com>
2025-05-25 22:36:56 +0800

    feat(InviteLinkRecord): 添加邀请链接记录模块，包含消息处理、通知处理、请求处理和响应处理功能，支持群聊和私聊的开关机制，优化模块结构和功能说明

---
+++ app/modules/InviteLinkRecord/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+
+# 模块名称
+MODULE_NAME = "InviteLinkRecord"
+
+# 模块开关名称
+SWITCH_NAME = "ILR"
+
+# 模块描述
+MODULE_DESCRIPTION = "邀请链记录"
+
+# 数据目录
+DATA_DIR = os.path.join("data", MODULE_NAME)
+os.makedirs(DATA_DIR, exist_ok=True)
+
+
+# 模块的一些命令可以在这里定义，方便在其他地方调用，提高代码的复用率
+# ------------------------------------------------------------
+VIEW_INVITE_RECORD = "查看邀请"
+# ------------------------------------------------------------
