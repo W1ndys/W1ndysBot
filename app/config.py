ef2578a0769636257a70c4cccbad8e4058e8c2df
W1ndys <w1ndys@outlook.com>
2025-05-26 17:34:07 +0800

    feat(config): 添加飞书机器人URL和Secret配置

--- app/config.py
+++ app/config.py
@@ -10,9 +10,11 @@ WS_URL = "ws://192.168.229.133:3001"
 TOKEN = ""
 
 # 飞书机器人URL，选填，掉线时使用
-FEISHU_BOT_URL = ""
+FEISHU_BOT_URL = (
+    "https://open.feishu.cn/open-apis/bot/v2/hook/55648a44-6e84-4d8c-af16-30065ffba8c1"
+)
 
 # 飞书机器人Secret，选填，掉线时使用
-FEISHU_BOT_SECRET = ""
+FEISHU_BOT_SECRET = "Z1TZCSlTgDFDEwnUYXeRDh"
 
 # ==================== 配置项结束 ====================
