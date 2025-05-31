a2754a189664eb8df254a25c7189fe9b5cdbeaf6
W1ndys <w1ndys@outlook.com>
2025-05-31 19:23:43 +0800

    fix: 更新配置文件以直接从环境变量读取值，移除默认值设置，调整日志级别为INFO

--- app/config.py
+++ app/config.py
@@ -7,21 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 机器人顶级管理员QQ，必填
-OWNER_ID = os.getenv("OWNER_ID", "482548878")
+OWNER_ID = os.getenv("OWNER_ID")
 
 # 连接到NapCatQQ的WebSocket连接地址，默认使用3001端口
-WS_URL = os.getenv("WS_URL", "ws://192.168.229.133:3001")
+WS_URL = os.getenv("WS_URL")
 
 # 连接到NapCatQQ的机器人Token
-TOKEN = os.getenv("TOKEN", "")
-
-# 日志等级，可选值：DEBUG, INFO, WARNING, ERROR, CRITICAL
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+TOKEN = os.getenv("TOKEN")
 
 # 飞书机器人URL，选填，掉线时使用
-FEISHU_BOT_URL = os.getenv("FEISHU_BOT_URL", "")
+FEISHU_BOT_URL = os.getenv("FEISHU_BOT_URL")
 
 # 飞书机器人Secret，选填，掉线时使用
-FEISHU_BOT_SECRET = os.getenv("FEISHU_BOT_SECRET", "")
+FEISHU_BOT_SECRET = os.getenv("FEISHU_BOT_SECRET")
 
 # ==================== 配置项结束 ====================
